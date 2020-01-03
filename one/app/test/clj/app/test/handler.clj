58104a796b7e359e37946f787073e5a399732094
Sabda PS <sabdaps@gmail.com>
2020-01-04 02:18:48 +0700

    lumixone

---
+++ one/app/test/clj/app/test/handler.clj
@@ -0,0 +1,27 @@
+(ns app.test.handler
+  (:require
+    [clojure.test :refer :all]
+    [ring.mock.request :refer :all]
+    [app.handler :refer :all]
+    [app.middleware.formats :as formats]
+    [muuntaja.core :as m]
+    [mount.core :as mount]))
+
+(defn parse-json [body]
+  (m/decode formats/instance "application/json" body))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (mount/start #'app.config/env
+                 #'app.handler/app)
+    (f)))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
